0196746403c5e275bad899e53e37c5b99311aa3f
Charlie Somerville <charlie@charliesomerville.com>
2016-04-04 11:46:48 -0700

    rename count to size in memzero

--- kernel/string.c
+++ kernel/string.c
@@ -18,20 +18,20 @@ memmove(void* dst_, const void* src_, size_t n)
 }
 
 void
-memzero(void* ptr, size_t count)
+memzero(void* ptr, size_t size)
 {
-    if (count % 8 == 0) {
-        count /= 8;
+    if (size % 8 == 0) {
+        size /= 8;
 
         uint64_t* p64 = ptr;
 
-        for (uint64_t i = 0; i < count; i++) {
+        for (uint64_t i = 0; i < size; i++) {
             p64[i] = 0;
         }
     } else {
         uint8_t* p8 = ptr;
 
-        for (size_t i = 0; i < count; i++) {
+        for (size_t i = 0; i < size; i++) {
             p8[i] = 0;
         }
     }
